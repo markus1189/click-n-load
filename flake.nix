0d52fe3191594ad5b463491fda5bcfa78c62dd18
Markus Hauck <markus1189@gmail.com>
2022-05-26 10:23:10 +0200

    Use flakes

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Flake for dev shell";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        myHaskellPackages = ps:
+          with ps; [
+            base64-bytestring
+            either
+            hex-text
+            language-javascript
+            lens
+            servant-server
+          ];
+        myGhc = pkgs.haskellPackages.ghcWithHoogle myHaskellPackages;
+        clickNLoad = pkgs.writeScriptBin "click-n-load" ''
+          ${myGhc}/bin/runhaskell ${./clicknload-decryptor.hs}
+        '';
+      in {
+        defaultApp = clickNLoad;
+
+        devShell = with pkgs;
+          pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              myGhc
+              (haskellPackages.haskell-language-server)
+              clickNLoad
+            ];
+          };
+      });
+}
